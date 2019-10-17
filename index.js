6da2fc6c25214099fcc7a08820ce00949d647b9d
chiragmodi1247 <mchirag1247@gmail.com>
2019-10-17 13:44:40 +0530

    Logged in and redirected

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 console.log("scripted success");
+function login() {
+  var name = document.getElementById("user_email").value;
+  var pwd = document.getElementById("user_pwd").value;
+  //   console.log("Logged!!");
+  //   console.log("Name is " + name + " and this is pwd " + pwd);
+  //   document.getElementById("user_email_dis").innerHTML = name;
+  //   document.getElementById("user_pwd_dis").innerHTML = pwd;
+
+  if (name === "this@email.com" && pwd === "thispass") {
+    location.replace(
+      "file:///F:/React%20-%20project/Gdmail/Screens/dashboard.html"
+    );
+  } else {
+    alert("Wrong credentials");
+  }
+}
