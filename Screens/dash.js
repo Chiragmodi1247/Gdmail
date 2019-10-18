f551784c3cab6f61229d00178f669ab0e2188de3
Chirag Modi <mchirag1247@gmail.com>
2019-10-19 02:50:25 +0530

    dynamic content loaded

--- Screens/dash.js
+++ Screens/dash.js
@@ -2,5 +2,80 @@ console.log("Dash script loaded");
 var x = sessionStorage.getItem("currentUsr");
 document.addEventListener("DOMContentLoaded", event => {
   document.getElementById("intro").innerHTML = "Hello " + x;
+  var header = document.getElementById("sidenav");
+  var btns = header.getElementsByClassName("menu-item");
+  for (var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function() {
+      var current = document.getElementsByClassName("active");
+      current[0].className = current[0].className.replace(" active", "");
+      this.className += " active";
+    });
+  }
+  var subject = [
+    "subject1",
+    "subject2",
+    "subject3",
+    "subject4",
+    "subject5",
+    "subject6",
+    "subject7",
+    "subject8",
+    "subject9",
+    "subject10",
+    "subject11",
+    "subject12",
+    "subject13",
+    "subject14",
+    "subject15",
+    "subject16",
+    "subject17",
+    "subject18",
+    "subject19",
+    "subject20"
+  ];
+  var desc = [
+    "1.It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layouta reader will be distracted by the readable content of a page when looking at its layout.",
+    "2.It is a long established fact that a reader will be distracted by the readable content of a page when looking at itsthe readable content of a page when looking at its layout layout.",
+    "3.It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.",
+    "4.It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.",
+    "5.It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.",
+    "6.It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.",
+    "7.It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.",
+    "8.It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.",
+    "9.It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.",
+    "10.It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.",
+    "11.It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.",
+    "12.It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.",
+    "13.It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.",
+    "14.It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.",
+    "15.It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.",
+    "16.It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.",
+    "17.It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.",
+    "18.It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.",
+    "19.It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.",
+    "20.It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout."
+  ];
+
+  // console.log(desc[8][132]);
+
+  // console.log(subject.length);
+  for (var j = 0; j < subject.length; j++) {
+    var node = document.createElement("div");
+    var sub = document.createElement("span");
+    var des = document.createElement("span");
+    node.className = "inbox";
+    sub.className = "item1";
+    des.className = "item2";
+    sub.innerHTML = subject[j];
+    if (desc[j].length > 150) {
+      var res = desc[j].substring(0, 147);
+      res += "...";
+      des.innerHTML = res;
+    } else {
+      des.innerHTML = desc[j];
+    }
+    node.appendChild(sub);
+    node.appendChild(des);
+    document.getElementById("data").appendChild(node);
+  }
 });
-console.log("git trial");
